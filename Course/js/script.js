eac0cea9ab80dcd307ff2b5a263b630b37caa7cc
Yura <verbitskey@gmail.com>
2022-01-29 19:22:07 +0200

    Java script

---
+++ Course/js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+// const result = confirm("Are you here?");
+// console.log(result);
+// const answer = +prompt("Вам есть 18", "18");
+// console.log((answer+5));
+const answers = [];
+// answers[0] = prompt('What is you name?', '');
+// answers[1] = prompt('What is you secondname?', '');
+// answers[2] = prompt('How old are you?', ''); 
+console.log(typeof(answers));  
+    // alert("Hello");
+// let number = 5;
+// let a = 15;
+// console.log(a);
+// const leftBorderWidth = 1;
+
+// number = 10;
+
+// const obj = {
+//     a:50
+// };
+// obj.a = 10;
+// console.log(obj);
+
+// console.log(name);
+// var name = 'Ivan';
+
+// {
+//     let result = 50;
+// }
+// alert(5);
+// [].push('a');
+// console.log(4 / 0);
+// console.log('string' * 9);
+console.log(1);
+//console.log(something);
+let und;
+console.log(und);
+let arr = ["ggg", "dfd"];
+console.log(arr[0]);
